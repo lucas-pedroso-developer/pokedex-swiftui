d5a4ada930adf6f349af0ef4bce5ea3d0304a0ca
Lucas Daniel Pedroso <lucaspedroso@brq.com>
2021-08-31 14:53:53 -0300

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by user on 31/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
